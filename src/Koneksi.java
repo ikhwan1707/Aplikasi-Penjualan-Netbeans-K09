ce1439afab744648cee32c4fff5eb13415d103e8
ajun02 <fauzanalkhairi48@gmail.com>
2023-11-09 11:24:40 +0700

    -formbrgmasuk -salsa -formbarang

--- src/Koneksi.java
+++ src/Koneksi.java
@@ -1,37 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 
 import java.sql.Connection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 
 /**
  *
- * @author SMKI Utama3
+ * @author user
  */
 public class Koneksi {
-    public static Connection Koneksi;
-    public static Connection getKoneksi(){
-        // cek apakah koneksi null
-        if (Koneksi == null)
+     public static Connection koneksi;
+     public static Connection getKoneksi(){
+         //cek apakah koneksi null
+         if (koneksi == null)
         {
             try {
-                String url = "jdbc:mysql://localhost:3306/db_netbeans";
+                String url = "jdbc:mysql://localhost:3306/db_penjualan_barang_pas_xiib";
                 String user = "root";
                 String password = "";
                 DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
-                Koneksi = DriverManager.getConnection(url, user, password);
+                koneksi = DriverManager.getConnection(url, user, password);
                 System.out.print("Berhasil Membuat Koneksi");
             } catch (SQLException t) {
                 System.out.println("Error Membuat Koneksi");
             }
         }
-        return Koneksi;
+        return koneksi;
     }
-
 }
