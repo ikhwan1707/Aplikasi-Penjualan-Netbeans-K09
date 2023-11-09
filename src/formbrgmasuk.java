9dd130fccf45bed6625de6a0ffc356463f41ae6b
salsabila <salsabil123344@gmail.com>
2023-11-09 11:15:00 +0700

    -formbrgmasuk -salsa -formbarang

--- src/formbrgmasuk.java
+++ src/formbrgmasuk.java
@@ -43,26 +43,26 @@ public class formbrgmasuk extends javax.swing.JFrame {
         jLabel14 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jTextField3 = new javax.swing.JTextField();
-        jComboBox2 = new javax.swing.JComboBox<>();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
-        jComboBox3 = new javax.swing.JComboBox<>();
-        jTextField6 = new javax.swing.JTextField();
-        jTextField7 = new javax.swing.JTextField();
-        jTextField8 = new javax.swing.JTextField();
-        jTextField9 = new javax.swing.JTextField();
-        jTextField10 = new javax.swing.JTextField();
+        txttglmsk = new javax.swing.JTextField();
+        txtnonota = new javax.swing.JTextField();
+        cmbidpetugas = new javax.swing.JComboBox<>();
+        txtnamaptgs = new javax.swing.JTextField();
+        cmbiddstr = new javax.swing.JComboBox<>();
+        txtnmdstr = new javax.swing.JTextField();
+        txtkota = new javax.swing.JTextField();
+        cmbkode = new javax.swing.JComboBox<>();
+        txtnmbrg = new javax.swing.JTextField();
+        txtharga = new javax.swing.JTextField();
+        txtstok = new javax.swing.JTextField();
+        txtjumlah = new javax.swing.JTextField();
+        txtsbtotal = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jLabel15 = new javax.swing.JLabel();
         jLabel16 = new javax.swing.JLabel();
-        jTextField11 = new javax.swing.JTextField();
+        txttotal = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -146,25 +146,25 @@ public class formbrgmasuk extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTable1);
 
         jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 460, 710, 120));
-        jPanel1.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 80, 210, -1));
-        jPanel1.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 110, 210, -1));
+        jPanel1.add(txttglmsk, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 80, 210, -1));
+        jPanel1.add(txtnonota, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 110, 210, -1));
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 140, 210, -1));
-        jPanel1.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 170, 210, -1));
+        cmbidpetugas.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Silahkan Pilih ID" }));
+        jPanel1.add(cmbidpetugas, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 140, 210, -1));
+        jPanel1.add(txtnamaptgs, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 170, 210, -1));
 
-        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox2, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 200, 210, -1));
-        jPanel1.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 230, 210, -1));
-        jPanel1.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 260, 210, -1));
+        cmbiddstr.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Silahkan pilih ID", " " }));
+        jPanel1.add(cmbiddstr, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 200, 210, -1));
+        jPanel1.add(txtnmdstr, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 230, 210, -1));
+        jPanel1.add(txtkota, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 260, 210, -1));
 
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        jPanel1.add(jComboBox3, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 340, 200, -1));
-        jPanel1.add(jTextField6, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 370, 200, -1));
-        jPanel1.add(jTextField7, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 400, 200, -1));
-        jPanel1.add(jTextField8, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 330, 70, -1));
-        jPanel1.add(jTextField9, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 360, 70, -1));
-        jPanel1.add(jTextField10, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 400, 130, -1));
+        cmbkode.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Silahkan pilih Kode" }));
+        jPanel1.add(cmbkode, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 340, 200, -1));
+        jPanel1.add(txtnmbrg, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 370, 200, -1));
+        jPanel1.add(txtharga, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 400, 200, -1));
+        jPanel1.add(txtstok, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 330, 70, -1));
+        jPanel1.add(txtjumlah, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 360, 70, -1));
+        jPanel1.add(txtsbtotal, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 400, 130, -1));
 
         jButton1.setText("jButton1");
         jPanel1.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 360, -1, -1));
@@ -183,7 +183,7 @@ public class formbrgmasuk extends javax.swing.JFrame {
 
         jLabel16.setText("Rp");
         jPanel1.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 590, -1, -1));
-        jPanel1.add(jTextField11, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 590, 100, -1));
+        jPanel1.add(txttotal, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 590, 100, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -235,13 +235,13 @@ public class formbrgmasuk extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cmbiddstr;
+    private javax.swing.JComboBox<String> cmbidpetugas;
+    private javax.swing.JComboBox<String> cmbkode;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JComboBox<String> jComboBox2;
-    private javax.swing.JComboBox<String> jComboBox3;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
@@ -262,16 +262,16 @@ public class formbrgmasuk extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField10;
-    private javax.swing.JTextField jTextField11;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
-    private javax.swing.JTextField jTextField6;
-    private javax.swing.JTextField jTextField7;
-    private javax.swing.JTextField jTextField8;
-    private javax.swing.JTextField jTextField9;
+    private javax.swing.JTextField txtharga;
+    private javax.swing.JTextField txtjumlah;
+    private javax.swing.JTextField txtkota;
+    private javax.swing.JTextField txtnamaptgs;
+    private javax.swing.JTextField txtnmbrg;
+    private javax.swing.JTextField txtnmdstr;
+    private javax.swing.JTextField txtnonota;
+    private javax.swing.JTextField txtsbtotal;
+    private javax.swing.JTextField txtstok;
+    private javax.swing.JTextField txttglmsk;
+    private javax.swing.JTextField txttotal;
     // End of variables declaration//GEN-END:variables
 }
